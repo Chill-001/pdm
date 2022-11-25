28456ae4584e131b45150158428c2e234adb7d2e
luna figueiredo <up202007414@up.pt>
2022-11-25 11:31:44 +0000

    class auxiliar

--- jersey-jetty/src/main/java/com/mkyong/Movie.java
+++ jersey-jetty/src/main/java/com/mkyong/Movie.java
@@ -1,2 +1,36 @@
-package com.mkyong;public class Movie {
+package com.mkyong;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Movie {
+
+    String name;
+    int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        try {
+            // takes advantage of toString() implementation to format {"a":"b"}
+            return new JSONObject().put("id", id).put("name", name).toString();
+        } catch (JSONException e) {
+            return null;
+        }
+    }
 }
